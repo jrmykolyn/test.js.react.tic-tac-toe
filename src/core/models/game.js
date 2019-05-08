fc9ace58ec7c0ced626e29d898a9b292410d2f70
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-05-07 21:19:33 -0400

    Add support for Game#play()

--- src/core/models/game.js
+++ src/core/models/game.js
@@ -3,4 +3,9 @@ export default class Game {
     this._state = [[], [], []];
     this._currentPlayer = 0;
   }
+
+  play(row, column) {
+    this._state[row][column] = this._currentPlayer;
+    this._currentPlayer = this._currentPlayer % 2 === 0 ? 1 : 0;
+  }
 }
--- test/unit/core/models/game.test.js
+++ test/unit/core/models/game.test.js
@@ -21,4 +21,39 @@ describe('Game', () => {
       expect(new Game()._currentPlayer).to.eq(0);
     });
   });
+
+  describe('Instance methods', () => {
+    let game;
+
+    beforeEach(() => {
+      game = new Game();
+    });
+
+    describe('play()', () => {
+      it('should apply the current player\'s identifier using the coordinates provided', () => {
+        const row = 0;
+        const column = 0;
+        const coords = [row, column];
+
+        game.play(...coords);
+
+        expect(game._state[row][column]).to.eq(0);
+      });
+
+      it('should alternate between players', () => {
+        const row1 = 0;
+        const column1 = 0;
+        const row2 = 0;
+        const column2 = 1;
+        const coords1 = [row1, column1];
+        const coords2 = [row2, column2];
+
+        game.play(...coords1);
+        game.play(...coords2);
+
+        expect(game._state[row1][column1]).to.eq(0);
+        expect(game._state[row2][column2]).to.eq(1);
+      });
+    });
+  });
 });
