da7243bc28ea2e58a99652c4e0aed60ff3d4778e
Jesse R Mykolyn <jesse.mykolyn@groupbyinc.com>
2019-05-08 22:10:28 -0400

    Update Cell component: render child node if selected

--- src/ui/Cell.js
+++ src/ui/Cell.js
@@ -3,11 +3,18 @@ import React from 'react';
 export default class Cell extends React.Component {
   render() {
     const baseClasses = ['cell'];
+    const hasVal = typeof this.props.val === 'number';
+    let innerElem = '';
 
-    if (typeof this.props.val === 'number') baseClasses.push(`player-${this.props.val}`);
+    if (hasVal) {
+      baseClasses.push(`player-${this.props.val}`);
+      innerElem = <span></span>
+    }
 
     return (
-      <div className={ baseClasses.join(' ' ) } onClick={ this.props.onClick }></div>
+      <div className={ baseClasses.join(' ') } onClick={ this.props.onClick }>
+        { innerElem }
+      </div>
     );
   }
 }
